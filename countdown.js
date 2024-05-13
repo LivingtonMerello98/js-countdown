579aa349c94990c4dfc23d46876c87cc58a7a842
LivingtonMerello98 <livington.merello@gmail.com>
2024-05-13 14:12:25 +0200

    [upd] function to start counter, and start countdown when page is loaded

--- countdown.js
+++ countdown.js
@@ -1,1 +1,18 @@
 'use strict'
+
+
+// Funzione per avviare il countdown
+function startCountdown() {
+    let seconds = 10;
+    const countdownInterval = setInterval(function() {
+      seconds--;
+
+      if (seconds < 0) {
+        clearInterval(countdownInterval);
+        alert('Buon anno!');
+      }
+    }, 1000);
+  }
+
+  // Avvia il countdown quando la pagina è pronta
+  document.addEventListener('DOMContentLoaded', startCountdown);
